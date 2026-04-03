d23a463e9bd9313c0cb102576a9a2c55cc3ca759
ViniGomes1 <vinig0014@gmail.com>
2026-04-03 20:45:40 -0300

    telegram scraping teste: 0.0.15

--- bot.lua
+++ bot.lua
@@ -39,9 +39,9 @@ local function process_update(update)
         if cmd == "/amazon" then
             print("o arg: " .. arg .. "o cmd: " .. cmd)
             local amazonClass = AmazonScraping:new("https://amzn.to/41cCZPD")
-            local root = amazonClass:parseHtmlCenter(false)
-            local urlImage = amazonClass:urlImage(root)
-            botBotado:send_photo(chat_id, urlImage, "algo", {parse_mode = 'HTML'})
+            local root = amazonClass:parseHtmlCenter()
+            local title = amazonClass:productTitle(root)
+            botBotado:send_photo(chat_id, "https://m.media-amazon.com/images/I/61A9XU5z-JL._AC_SX425_.jpg", title, {parse_mode = 'HTML'})
         else
             botBotado:send_message(chat_id, "Você disse: " .. text)
         end
