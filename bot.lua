7a7446b5b88307cd3fa49e6e211d9ce8dd29d841
ViniGomes1 <vinig0014@gmail.com>
2026-04-04 15:13:38 -0300

    adicionando funcionalidades: 0.0.1

--- bot.lua
+++ bot.lua
@@ -34,17 +34,27 @@ local function process_update(update)
         local chat_id = update.message.chat.id
         local text    = update.message.text or ""
         local cmd, arg = text:match("([^%s]+)%s+(.+)")
-
+        
         if cmd == "/amazon" then
-            local amazon = sendAmazonTextProduct(arg)
-            botBotado:send_photo(chat_id, amazon.imageUrl, amazon.text, {parse_mode = 'HTML'})
-        else
-            botBotado:send_message(chat_id, "Você disse: " .. text)
+            if string.match(arg, "https://amzn.to/") then
+                local amazon = sendAmazonTextProduct(arg)
+                botBotado:send_photo(chat_id, amazon.imageUrl, amazon.text, {parse_mode = 'HTML'})
+            else
+                botBotado:send_message(chat_id, "o link amazon é incompatível, deve conter pelo menos https://amzn.to/")
+            end
+        else if cmd == "/mercadolivre" then
+            botBotado:send_message(chat_id,"mercado livre está em desenvolvimento atualmente")
+        else if cmd == "/sair" then
+            botBotado:send_message(chat_id, "saindo(eu não sai verdadeiramente) &#1F47B; &#1F47B;", {parse_mode = 'HTML'})
+        end
+        end
+            botBotado:send_message(chat_id, "Primeiramente saiba que o bot tem apenas UMA funcionalidade, escolha ela abaixo \n" ..
+            "/mercado_livre {link} para fazer um anuncio mercado livre a partir de um link" ..
+            "/amazon {link} para fazer um anuncio amazon a partir de um link" ..
+            "/sair para sair")
         end
     end
 end
-
-
 
 
 return { process_update = process_update }
---
+++ switch_teste.lua
@@ -0,0 +1,7 @@
+local urlAz = "https://amzn.to/3PGoWz1"
+
+if string.match(urlAz, "https://amzn.to/") then
+    print("algo")
+else
+    print("nao")
+end
